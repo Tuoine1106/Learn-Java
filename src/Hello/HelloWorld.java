71b78545bebca1cda97b016d0469fb0398a590e4
TuoiNe1106 <cuquangtuoi11@gmail.com>
2024-12-16 20:23:38 +0700

    init project

---
+++ src/Hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package Hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Ngu học");
+		System.out.println("Ngu học");
+		System.out.println("Ngu học");
+		System.out.println("Ngu học");
+		System.out.println("Ngu học");
+		System.out.println("Ngu học");
+	}
+}
